1b691af7ed5d5535d5801ca09e623add4f88008a
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-04 11:34:18 +0100

    Setup better looking icon for starship

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -2,5 +2,12 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "[](bold green) ";
+	error_symbol = "[✗](bold red) ";
+      };
+    };
   };
 }
