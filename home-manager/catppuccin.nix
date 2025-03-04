fbc264bd2a510b373cac7608728e226ddee43cf6
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-04 12:23:33 +0100

    Remove theming from fzf and move to catppuccin config as well as enable for other programs

--- home-manager/catppuccin.nix
+++ home-manager/catppuccin.nix
@@ -2,7 +2,9 @@
   catppuccin = {
     flavor = "mocha";
     bat.enable = true;
-    starship.enable = true;
+    fzf.enable = true;
     kitty.enable = true;
+    lazygit.enable = true;
+    starship.enable = true;
   };
 }
