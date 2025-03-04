ed345f552c92d62686cab4e3ace609bfa3d36633
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-04 17:46:03 +0100

    Add settings for dunst to make it look just right

--- home-manager/dunst.nix
+++ home-manager/dunst.nix
@@ -1,5 +1,15 @@
 {
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        width = 600;
+	height = 300;
+	shrink = true;
+	offset = "30x50";
+	origin = "top-right";
+	font = "JetBrainsMono Nerd Font 12";
+      };
+    };
   };
 }
