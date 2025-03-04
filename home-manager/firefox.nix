637c2b2bcdcfcf91ea1d97735afd7c3f28e53133
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-04 22:25:09 +0100

    Setup unduck for searching in firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -10,13 +10,31 @@ in {
     profiles = {
       jaap = {
         id = 0;
-	name = "jaap";
-	isDefault = true;
-	extensions = firefoxExtensions;
-	settings = {
-	  "privacy.donottrackheader.enabled" = true;
-	  "browser.startup.page" = 3;
-	};
+		name = "jaap";
+		isDefault = true;
+		extensions = firefoxExtensions;
+		settings = {
+		  "privacy.donottrackheader.enabled" = true;
+		  "browser.startup.page" = 3;
+		};
+		search = {
+			engines = {
+				"Unduck" = {
+					urls = [
+					{
+						template = "https://unduck.link";
+						params = [
+						{ name = "q"; value = "{searchTerms}"; }
+						];
+					}
+					];
+					iconURL = "https://media.licdn.com/dms/image/v2/D5603AQG23cObe6YgcQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1723116164570?e=2147483647&v=beta&t=EYRl81Ra-xFzEZ2GyTrNuVjzlYF-ikZ9rc4vWqKzHyk";
+
+					definedDefault = true;
+				};
+			};
+		  default = "Unduck";
+		};
       };
     };
   };
