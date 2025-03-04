18e28ec88e660c88465247c3783c68dbc6133c35
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-04 11:53:33 +0100

    Added colors to fzf

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -1,5 +1,22 @@
 {
   programs.fzf = {
     enable = true;
+    colors = {
+      "bg+" = "#313244";
+      "bg" = "#1e1e2e";
+      "selected-bg" = "#45475a";
+      "hl" = "#f38ba8";
+      "hl+" = "#f38ba8";
+      "fg" = "#cdd6f4";
+      "fg+" = "#cdd6f4";
+      "border" = "#313244";
+      "header" = "#f38ba8";
+      "label" = "#cdd6f4";
+      "info" = "#cba6f7";
+      "pointer" = "#f5e0dc";
+      "marker" = "#b4befe";
+      "prompt" = "#cba6f7";
+      "spinner" = "#f5e0dc";
+    };
   };
 }
