90c18c99ffbdee3f2db2af150bc447428cd9054c
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-05 16:14:51 +0100

    Make pavucontrol float

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -76,6 +76,8 @@
           "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
           "float,class:(clipse)"
           "size 622 652,class:(clipse)"
+          "float,class:(pavucontrol)"
+          "size 800 600,class:(pavucontrol)"
         ];
 
         general = {
