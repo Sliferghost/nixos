897183145624ce4ebbe038750669c208c3359c0d
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-07 15:05:05 +0100

    Setup cmp with luasnip and friendly snippets

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -162,6 +162,50 @@
         };
       };
 
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+
+        settings = {
+          snippet = {
+            expand = ''
+              function(args)
+                require('luasnip').lsp_expand(args.body)
+              end
+            '';
+          };
+
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+
+          mapping = {
+            "<C-n>" = "cmp.mapping.select_next_item()";
+            "<C-p>" = "cmp.mapping.select_prev_item()";
+
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+
+            "<C-y>" = "cmp.mapping.confirm { select = true }";
+
+            "<C-Space>" = "cmp.mapping.complete {}";
+          };
+
+          completion.completeopt = "menu,menuone,noinsert";
+        };
+      };
+
+      friendly-snippets = {
+        enable = true;
+      };
+
+      luasnip = {
+        enable = true;
+      };
+
       which-key = {
         enable = true;
         settings = {
