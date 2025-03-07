fdd8d8cbde5395a92ee92718dac258189fe0df39
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-07 14:16:00 +0100

    Added some more plugins and config to nixvim

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -68,6 +68,54 @@
           desc = "Clear highlighting on search";
         };
       }
+      {
+        mode = [ "n" ];
+        key = "<leader>e";
+        action = {
+          __raw = ''
+            function()
+              MiniFiles.open()
+            end
+          '';
+        };
+        options = {
+          desc = "Open [E]xplorer";
+        };
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>/";
+        action = {
+          __raw = ''
+            function()
+              require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 10,
+                previewer = false,
+              })
+            end
+          '';
+        };
+        options = {
+          desc = "[/] Fuzzily search in current buffer";
+        };
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>s/";
+        action = {
+          __raw = ''
+            function()
+              require('telescope.builtin').live_grep {
+                grep_open_files = true,
+                prompt_title = 'Live Grep in Open Files',
+              }
+            end
+          '';
+        };
+        options = {
+          desc = "[S]earch [/] in Open Files";
+        };
+      }
     ];
 
     autoGroups = {
@@ -116,6 +164,123 @@
 
       which-key = {
         enable = true;
+        settings = {
+          spec = [
+            {
+              mode = [
+                "n"
+                "x"
+              ];
+              __unkeyed-1 = "<leader>c";
+              group = "[C]ode";
+            }
+            {
+              __unkeyed-1 = "<leader>d";
+              group = "[D]ocument";
+            }
+            {
+              __unkeyed-1 = "<leader>r";
+              group = "[R]ename";
+            }
+            {
+              __unkeyed-1 = "<leader>s";
+              group = "[S]earch";
+            }
+            {
+              __unkeyed-1 = "<leader>w";
+              group = "[W]orkspace";
+            }
+            {
+              __unkeyed-1 = "<leader>t";
+              group = "[T]oggle";
+            }
+            {
+              mode = [
+                "n"
+                "v"
+              ];
+              __unkeyed-1 = "<leader>h";
+              group = "Git [H]unk";
+            }
+          ];
+        };
+      };
+
+      telescope = {
+        enable = true;
+
+        extensions = {
+          fzf-native.enable = true;
+        };
+
+        keymaps = {
+          "<leader>sh" = {
+            action = "help_tags";
+            options.desc = "[S]earch [H]elp";
+          };
+          "<leader>sk" = {
+            action = "keymaps";
+            options.desc = "[S]earch [K]eymaps";
+          };
+          "<leader>sf" = {
+            action = "find_files";
+            options.desc = "[S]earch [F]iles";
+          };
+          "<leader>ss" = {
+            action = "builtin";
+            options.desc = "[S]earch [S]elect Telescope";
+          };
+          "<leader>sw" = {
+            action = "grep_string";
+            options.desc = "[S]earch current [W]ord";
+          };
+          "<leader>sg" = {
+            action = "live_grep";
+            options.desc = "[S]earch by [G]rep";
+          };
+          "<leader>sd" = {
+            action = "diagnostics";
+            options.desc = "[S]earch [D]iagnostics";
+          };
+          "<leader>sr" = {
+            action = "resume";
+            options.desc = "[S]earch [R]esume";
+          };
+          "<leader>s." = {
+            action = "oldfiles";
+            options.desc = "[S]earch Recent Files ('.' for repeat)";
+          };
+          "<leader><leader>" = {
+            action = "buffers";
+            options.desc = "[ ] Find existing buffers";
+          };
+        };
+      };
+
+      web-devicons = {
+        enable = true;
+      };
+
+      mini = {
+        enable = true;
+        modules = {
+          icons = { };
+          ai = {
+            n_lines = 500;
+          };
+          surround = { };
+          files = { };
+          statusline = {
+            use_icons = true;
+            section_location = {
+              __raw = ''
+                function()
+                  return '%2l:%-2v'
+                end
+              '';
+            };
+          };
+        };
       };
 
       conform-nvim = {
