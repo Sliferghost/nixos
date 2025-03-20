926a130bc88f23c6a87c22fb3090a7244beb9840
Jaap Oudejans <jaapoudejans@gmail.com>
2025-03-20 17:35:20 +0100

    Setup the mgba config

--- home-manager/gaming.nix
+++ home-manager/gaming.nix
@@ -4,4 +4,18 @@
     heroic
     mgba
   ];
+
+  xdg.configFile."mgba/config.ini".text = ''
+    [input]
+    keyA=X
+    keyB=Z
+    keyStart=Enter
+    keySelect=Backspace
+    keyL=A
+    keyR=S
+    keyUp=Up
+    keyDown=Down
+    keyLeft=Left
+    keyRight=Right
+  '';
 }
