03adf9ba275f0a3b174367da0a4ca0588e7280c9
Jaap Oudejans <jaapoudejans@gmail.com>
2025-05-10 11:40:56 +0200

    Updated to the latest version of godot and added some rules for godot in hyprland

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -95,6 +95,12 @@
           "center,class:(Godot),title:^(Create New Node)$"
           "size 65% 50%,class:(Godot),title:^(Project Settings)$"
           "center,class:(Godot),title:^(Project Settings)$"
+          "size 65% 50%,class:(Godot),title:^(Save.*)$"
+          "center,class:(Godot),title:^(Save.*)$"
+          "size 50% 60%,class:(Godot),title:^(Attach Node Script)$"
+          "center,class:(Godot),title:^(Attach Node Script)$"
+          "size 50% 60%,class:(Godot),title:^(Connect a Signal to a Method)$"
+          "center,class:(Godot),title:^(Connect a Signal to a Method)$"
         ];
 
         general = {
