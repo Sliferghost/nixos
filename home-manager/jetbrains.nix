66db019863fb403a0e08edb9f3b7aed26fc51b36
Jaap Oudejans <jaapoudejans@gmail.com>
2025-07-22 10:29:16 +0200

    Updated to the latest version

--- home-manager/jetbrains.nix
+++ home-manager/jetbrains.nix
@@ -1,7 +1,9 @@
 { unstable, ... }:
 {
   home.packages = with unstable; [
-    jetbrains.idea-ultimate
+    (jetbrains.idea-ultimate.override {
+      jdk = unstable.openjdk21;
+    })
   ];
 
   home.sessionVariables = {
